2f88cdd7d908455bb0d0a576b528a1d2dc125f75
limbohunt <omidxplimbo@gmail.com>
2023-06-03 13:30:20 +0330

    change remove alert

--- flows/recon/general.go
+++ flows/recon/general.go
@@ -4,9 +4,8 @@ import (
 	"context"
 	"fmt"
 	"github.com/omidxplimbo/mustache/config"
+	"github.com/omidxplimbo/mustache/db"
 	"github.com/omidxplimbo/mustache/logger"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 	"gopkg.in/yaml.v3"
 	"io/ioutil"
 	"os/exec"
@@ -53,32 +52,20 @@ func General(projectName string, target string) {
 func runCommands(data map[string]interface{}, projectName string, domain string) {
 
 	// Extract database and collection names from YAML data
-	databaseName, ok := data["database"].(string)
-	if !ok {
-		fmt.Println("Invalid database name")
-		return
-	}
-	collectionName, ok := data["collection"].(string)
-	if !ok {
-		fmt.Println("Invalid collection name")
-		return
-	}
+	collectionName := data["collection"].(string)
 
-	// Create MongoDB session and collection
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil {
-		fmt.Printf("MongoDB client error: %v\n", err)
-		return
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), 10000*time.Second)
+	// Get project config
+	configProject := config.ProjectConfig()
+
+	// Connect to database
+	client, _ := db.ConnectToDatabase()
+
+	// Check if database exists
+	dbName := configProject.DbPrefix + projectName
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(configProject.DbTimeout)*time.Second)
 	defer cancel()
-	err = client.Connect(ctx)
-	if err != nil {
-		fmt.Printf("MongoDB connection error: %v\n", err)
-		return
-	}
-	defer client.Disconnect(ctx)
-	collection := client.Database(databaseName).Collection(collectionName)
+
+	collection := client.Database(dbName).Collection(collectionName)
 
 	var subdomains []subdomain // Declare subdomains variable outside of loop
 
@@ -130,12 +117,8 @@ func runCommands(data map[string]interface{}, projectName string, domain string)
 
 // Replace variables in the command
 func replaceVariables(command string, projectName string, domain string) string {
-
-	projectConfig := config.ProjectConfig()
 	command = strings.ReplaceAll(command, "$domain$", domain)
 	command = strings.ReplaceAll(command, "$projectName$", projectName)
-	command = strings.ReplaceAll(command, "$database$", projectConfig.DbPrefix+projectName)
-
 	return command
 }
 
