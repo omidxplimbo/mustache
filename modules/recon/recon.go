a5466238ab6500d1635c4e9cdb6a34802b210bde
limbohunt <omidxplimbo@gmail.com>
2023-06-03 12:04:03 +0330

    some change and add example of yaml file

--- modules/recon/recon.go
+++ modules/recon/recon.go
@@ -8,11 +8,16 @@ import (
 
 type Project struct{}
 
-func (p Project) General(projectName string) {
+func (p Project) General(projectName string, domain string) {
 	if projectName == "" {
 		log.Fatal("Project flag is required. Please use -h for help.")
 		os.Exit(0)
 	}
+	if domain == "" {
+		log.Fatal("Domain flag is required. Please use -h for help.")
+		os.Exit(0)
+	}
+
 	// send request to flow if exists
-	recon.General(projectName)
+	recon.General(projectName, domain)
 }
