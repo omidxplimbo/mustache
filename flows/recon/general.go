525aedea2c84cf2b915a343410c1527f07d97ba2
limbohunt <omidxplimbo@gmail.com>
2023-06-03 16:00:48 +0330

    general flow

--- flows/recon/general.go
+++ flows/recon/general.go
@@ -1,160 +1,43 @@
 package recon
 
 import (
-	"context"
 	"fmt"
 	"github.com/omidxplimbo/mustache/config"
-	"github.com/omidxplimbo/mustache/db"
+	recon "github.com/omidxplimbo/mustache/flows/recon/routines"
 	"github.com/omidxplimbo/mustache/logger"
 	"gopkg.in/yaml.v3"
 	"io/ioutil"
-	"os/exec"
-	"path/filepath"
-	"strings"
 	"time"
 )
-
-type subdomain struct {
-	Subdomain   string    `bson:"subdomain"`
-	Created     time.Time `bson:"created_at"`
-	Updated     time.Time `bson:"updated_at"`
-	Cidr        string    `bson:"cidr"`
-	Asn         string    `bson:"asn"`
-	Cdn         string    `bson:"cdn"`
-	HttpService bool      `bson:"httpService"`
-}
 
 func General(projectName string, target string) {
 
 	projectConfig := config.ProjectConfig()
 	logger.Info(fmt.Sprintf("Running general flow at: %s", time.Now().Format(projectConfig.TimeShow)))
 
-	// List YAML files in a directory
-	yamlFiles, err := filepath.Glob("flows/recon/configs/general/*.yaml")
-	if err != nil || len(yamlFiles) == 0 {
+	// Check yaml files for get all routines in the general flow
+	generalFlow, err := ioutil.ReadFile("config/base-routine/general.yaml")
+	if err != nil {
 		logger.Fetal("Yaml Configuration not exist")
 	}
 
-	// Parse each YAML file
-	for _, file := range yamlFiles {
-
-		// Read the YAML file
-		yamlFile, err := ioutil.ReadFile(file)
-		if err != nil {
-			logger.Fetal("Yaml Configuration not exist")
-		}
-
-		// Unmarshal the YAML into a map[string]interface{}
-		var data map[string]interface{}
-		err = yaml.Unmarshal(yamlFile, &data)
-		if err != nil {
-			logger.Fetal("Yaml Configuration not exist")
-		}
-
-		logger.Info(fmt.Sprintf("Running routin flow %s at: %s", data["name"].(string), time.Now().Format(projectConfig.TimeShow)))
-
-		// Execute commands
-		runYaml(data, projectName, target)
-
-		logger.Info(fmt.Sprintf("General flow done at: %s", time.Now().Format(projectConfig.TimeShow)))
-	}
-}
-
-// Execute commands in the YAML data
-func runYaml(data map[string]interface{}, projectName string, domain string) {
-
-	// Extract database and collection names from YAML data
-	collectionName := data["collection"].(string)
-
-	// Get project config
-	configProject := config.ProjectConfig()
-
-	// Connect to database
-	client, _ := db.ConnectToDatabase()
-
-	// Check if database exists
-	dbName := configProject.DbPrefix + projectName
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(configProject.DbTimeout)*time.Second)
-	defer cancel()
-
-	// Get database
-	collection := client.Database(dbName).Collection(collectionName)
-
-	var subdomains []subdomain // Declare subdomains variable outside of loop
-
-	commands, ok := data["command"].([]interface{})
-	if !ok {
-		return
-	}
-
-	for _, cmd := range commands {
-		command, ok := cmd.(string)
-		if !ok {
-			fmt.Println("Invalid command format")
-			continue
-		}
-
-		// Replace variables in the command
-		command = replaceVariables(command, projectName, domain)
-
-		// Run the command
-		logger.Info(fmt.Sprintf("Running Process: " + command))
-		output, err := executeCommand(command)
-		if err != nil {
-			fmt.Printf("Command execution error: %v\n", err)
-			continue
-		}
-		logger.Info(fmt.Sprintf("Run Process Successfully: " + command))
-
-		switch data["name"].(string) {
-		case "subdomain":
-			// Extract subdomains from command output
-			for _, line := range strings.Split(output, "\n") {
-				if line == "" {
-					continue
-				}
-				subdomain := subdomain{
-					Subdomain:   strings.TrimSpace(line),
-					Created:     time.Now(),
-					Updated:     time.Now(),
-					Cidr:        "",
-					Asn:         "",
-					Cdn:         "",
-					HttpService: false,
-				}
-				subdomains = append(subdomains, subdomain)
-			}
-		default:
-			return
-		}
-	}
-
-	if len(subdomains) > 0 {
-		// Insert subdomains into MongoDB collection
-		for _, subdomain := range subdomains {
-			_, err := collection.InsertOne(ctx, subdomain)
-			if err != nil {
-				logger.Fetal(fmt.Sprintf("MongoDB insert error: %v\n", err))
-			}
-		}
-	}
-	logger.Info(fmt.Sprintf("Add data to database done at: %s", time.Now().Format(configProject.TimeShow)))
-
-}
-
-// Replace variables in the command
-func replaceVariables(command string, projectName string, domain string) string {
-	command = strings.ReplaceAll(command, "$domain$", domain)
-	command = strings.ReplaceAll(command, "$projectName$", projectName)
-	return command
-}
-
-// Execute a command and return its output
-func executeCommand(command string) (string, error) {
-	cmd := exec.Command("bash", "-c", command)
-	output, err := cmd.Output()
+	// Unmarshal the YAML into a map[string]interface{}
+	var data map[string]interface{}
+	err = yaml.Unmarshal(generalFlow, &data)
 	if err != nil {
-		return "", err
+		logger.Fetal("Yaml Configuration not exist")
 	}
-	return string(output), nil
+
+	logger.Info(fmt.Sprintf("Running routin flow %s at: %s", data["name"].(string), time.Now().Format(projectConfig.TimeShow)))
+
+	for _, routine := range data["routine"].([]interface{}) {
+		switch routine {
+		case "subdomain":
+			recon.SubDomain(projectName, target)
+		default:
+			logger.Warning(fmt.Sprintf("There isn't any routins in the general flow"))
+		}
+	}
+
+	logger.Info(fmt.Sprintf("General flow done at: %s", time.Now().Format(projectConfig.TimeShow)))
 }
