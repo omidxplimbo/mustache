673e2e52fb98165085cd4a976fec0db0736c429f
limbohunt <omidxplimbo@gmail.com>
2023-06-05 20:38:04 +0330

    add get subdomain watch

--- db/subdomain.go
+++ db/subdomain.go
@@ -270,9 +270,10 @@ func (s Subdomain) GetSub(projectName string, target string) {
 
 	defer cursor.Close(context.Background())
 
-	if cursor.ID() == 0 {
+	if !cursor.Next(context.Background()) {
 		logger.Warning("There isn't any subdomain with this name for project")
 	}
+
 	logger.Info(fmt.Sprintf("Get information of %s subdomains for %s project: ", target, projectName))
 	for cursor.Next(context.Background()) {
 		var result bson.M
